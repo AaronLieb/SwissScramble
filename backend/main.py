e745ebbc329533503544c473cba75e270686060c
Aaron Lieberman <aarolieb@amazon.com>
2025-03-02 12:19:21 -0800

    Add scheduler for incomes

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
 import logging
+import schedule
+import threading
+import time
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
@@ -17,11 +21,36 @@ logging.getLogger("passlib").setLevel(logging.ERROR)
 logger = logging.getLogger(__name__)
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    create_db_and_tables()
+
+    logger.info("Creating scheduler")
+    schedule.every().hour.at(":00").do(give_income)
+    stop_schedule_thread = start_schedule_thread()
+
+    yield
+    stop_schedule_thread.set()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(game_router)
 
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+def start_schedule_thread(interval=1):
+    stop = threading.Event()
+
+    class ScheduleThread(threading.Thread):
+        def run(self):
+            while not stop.is_set():
+                schedule.run_pending()
+                time.sleep(interval)
+
+    continuous_thread = ScheduleThread()
+    continuous_thread.start()
+    return stop
+
+
+def give_income():
+    logger.info("give income")
