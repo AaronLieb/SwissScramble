e6a0694b62d187aecca439ce6614a13c72773d33
Isaac Feldman <isaaccfeldman@gmail.com>
2025-03-04 21:14:39 -0800

    feat: autoload test data into db

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,13 +1,24 @@
 from typing import Annotated
 from fastapi import Depends
 from sqlmodel import Session, SQLModel, create_engine
+import sqlite3
 
 from ..config import settings
 
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+    load_default_data()
 
+def load_default_data():
+    with open('scripts/load-data.sql', 'r') as sql_file:
+        sql_script = sql_file.read()
+
+        db = sqlite3.connect('main.db')
+        cursor = db.cursor()
+        cursor.executescript(sql_script)
+        db.commit()
+        db.close()
 
 def get_session():
     with Session(engine) as session:
