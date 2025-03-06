733295200323f059e13478c12ee779f53c768cee
Isaac Feldman <isaaccfeldman@gmail.com>
2025-03-05 21:19:13 -0800

    feat: login page with routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import Switzerland from './Switzerland.jsx'
+import Login from './Login.jsx'
 import {
   Grid2
 } from "@mui/material";
 import './App.css'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { red, purple } from '@mui/material/colors';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 const theme = createTheme({
   palette: {
@@ -18,23 +18,34 @@ const theme = createTheme({
 function App() {
 
 
-  return (
-    <div>
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={
+                           <div>
+                               <Grid2 container
+                                      id="home"
+                               >
+                               </Grid2>
+                               <Grid2 container>
+                                   <Grid2 item xs={0} lg={3}></Grid2>
+                                   <Grid2 item xs={12}>
+                                       <ThemeProvider theme={theme}>
+                                           <Switzerland/>
+                                       </ThemeProvider>
+                                   </Grid2>
+                                   <Grid2 item xs={0} lg={3}></Grid2>
+                               </Grid2>
+                           </div>
+                       }/>
+                <Route path='/login' element={
+                           <ThemeProvider theme={theme}>
+                               <Login/>
+                           </ThemeProvider>
+                                             }/>
+            </Routes>
+        </Router>
 
-      <Grid2 container
-        id="home"
-      >
-      </Grid2>
-      <Grid2 container>
-        <Grid2 item xs={0} lg={3}></Grid2>
-        <Grid2 item xs={12}>
-          <ThemeProvider theme={theme}>
-            <Switzerland />
-          </ThemeProvider>
-        </Grid2>
-        <Grid2 item xs={0} lg={3}></Grid2>
-      </Grid2>
-    </div>
   )
 }
 
