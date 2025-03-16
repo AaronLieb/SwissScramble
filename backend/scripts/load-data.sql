ac8f69a5f2290656fa3d57b7ec40b3dc21281905
Isaac Feldman <icf@amazon.com>
2025-03-16 13:28:55 -0700

    add: update starting canton values

--- backend/scripts/load-data.sql
+++ backend/scripts/load-data.sql
@@ -1,35 +1,35 @@
-INSERT INTO canton (name, value, level, destroyed, team_id) VALUES
-    ('Aargau', 1, 1, false, 1),
-    ('Appenzell Ausserrhoden', 1, 1, false, 1),
-    ('Appenzell Innerrhoden', 1, 3, false, 1),
-    ('Basel-Landschaft', 1, 1, false, 1),
-    ('Basel-Stadt', 1, 2, false, 1),
-    ('Bern', 1, 1, false, 1),
-    ('Fribourg / Freiburg', 1, 1, false, 1),
-    ('Geneva', 1, 1, false, 1),
-    ('Glarus', 1, 1, false, 1),
-    ('Grisons', 1, 2, false, 1),
-    ('Jura', 1, 1, false, 1),
-    ('Lucerne', 1, 1, false, 1),
-    ('Neuchatel', 1, 1, false, 1),
-    ('Nidwalden', 1, 1, false, 1),
-    ('Obwalden', 1, 1, false, 1),
-    ('Schaffhausen', 1, 1, false, 1),
-    ('Schwyz', 1, 1, false, 1),
-    ('Solothurn', 1, 3, false, 1),
-    ('St. Gallen', 1, 1, false, 2),
-    ('Thurgau', 1, 1, false, 2),
-    ('Ticino', 1, 1, false, 2),
-    ('Uri', 1, 1, false, 2),
-    ('Valais', 1, 1, false, 2),
-    ('Vaud', 1, 1, false, 2),
-    ('Zug', 1, 1, false, 2),
-    ('Zurich', 1, 2, false, 2);
+INSERT or IGNORE INTO canton (name, value, level, destroyed, team_id) VALUES
+    ('Aargau', 1, 0, false, 0),
+    ('Appenzell Ausserrhoden', 1, 0, false, 0),
+    ('Appenzell Innerrhoden', 1, 0, false, 0),
+    ('Basel-Landschaft', 1, 0, false, 0),
+    ('Basel-Stadt', 1, 0, false, 0),
+    ('Bern', 1, 0, false, 0),
+    ('Fribourg / Freiburg', 1, 0, false, 0),
+    ('Geneva', 1, 0, false, 0),
+    ('Glarus', 1, 0, false, 0),
+    ('Grisons', 1, 0, false, 0),
+    ('Jura', 1, 0, false, 0),
+    ('Lucerne', 1, 0, false, 0),
+    ('Neuchatel', 1, 0, false, 0),
+    ('Nidwalden', 1, 0, false, 0),
+    ('Obwalden', 1, 0, false, 0),
+    ('Schaffhausen', 1, 0, false, 0),
+    ('Schwyz', 1, 0, false, 0),
+    ('Solothurn', 1, 0, false, 0),
+    ('St. Gallen', 1, 0, false, 0),
+    ('Thurgau', 1, 0, false, 0),
+    ('Ticino', 1, 0, false, 0),
+    ('Uri', 1, 0, false, 0),
+    ('Valais', 1, 0, false, 0),
+    ('Vaud', 1, 0, false, 0),
+    ('Zug', 1, 0, false, 0),
+    ('Zurich', 1, 0, false, 0);
 
-INSERT INTO team (name, money, score, curses, challenges, income) VALUES ('A team', 0, 0, 0, 0, 150);
-INSERT INTO team (name, money, score, curses, challenges, income) VALUES ('Timsaac', 0, 0, 0, 0, 50);
+INSERT or IGNORE INTO team (name, money, score, curses, challenges, income) VALUES ('A team', 0, 0, 0, 0, 150);
+INSERT or IGNORE INTO team (name, money, score, curses, challenges, income) VALUES ('Timsaac', 0, 0, 0, 0, 50);
 
-INSERT INTO user (username, firstname, lastname, team_id, hashed_password) VALUES
+INSERT or IGNORE INTO user (username, firstname, lastname, team_id, hashed_password) VALUES
    -- remove these before starting the game, create users through api
    ('aaronlieb', 'Aaron', 'Lieberman', 1, '$2b$12$D.Sp6Eavi4YrE0gZ8fC.n.LEkxkvDRosaWSZYlDXQgw9vuzVLN1ne'),
    ('antonio', 'Antonio', 'Alonso', 1, '$2b$12$6yGm3YcfVi9INTsxbvVwo.kgoZyeGHx7SoQsSuGLquVrVxgksTOfy'),
