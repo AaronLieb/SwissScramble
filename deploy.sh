91293e98637aa2bde72e6455cad59d5cc6f4bf59
Isaac Feldman <isaaccfeldman@gmail.com>
2025-03-16 11:34:39 -0700

    fix: minor deploy script changes

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 # Deploy the container images to ECR.
 set -e
 # Variables
+DOCKER_BUILD="docker build --progress=plain"
 AWS_REGION="us-west-2"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 FRONTEND_REPO_NAME="swiss-frontend"
@@ -13,9 +14,11 @@ BACKEND_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${BACKEND_R
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 # Build Docker images
-docker build -t ${FRONTEND_IMAGE} ./frontend
-docker build -t ${BACKEND_IMAGE} ./backend
+$DOCKER_BUILD -t ${FRONTEND_IMAGE} ./frontend
+$DOCKER_BUILD -t ${BACKEND_IMAGE} ./backend
 
 # Push Docker images to ECR
 docker push ${FRONTEND_IMAGE}
 docker push ${BACKEND_IMAGE}
+ 
+aws ecs update-service --cluster swiss-scramble --service swiss-scramble-service --task-definition frontend-backend-task --force-new-deployment --region ${AWS_REGION}
